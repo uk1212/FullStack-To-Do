15a46e3616bbce9cab70b6e73627e0300e7697b7
Utkarsh Kumar <utkarsh.12may@gmail.com>
2023-05-22 19:43:33 +0530

    Added

--- data/database.js
+++ data/database.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 export const connectDb=()=>{mongoose.connect(process.env.MONGO_URI,{
     dbName: "backendapi"
 })
-.then(()=>{
-    console.log("Database connected!!");
+.then((c)=>{
+    console.log(`Database connected!! with ${c.connection.host}`)
 })
 .catch((e)=>console.log(e));
 };
